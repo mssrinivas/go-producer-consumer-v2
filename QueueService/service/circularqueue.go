0c1ed2bcafa8291957027a50c7c51b72e00bbd4d
M.S.Srinivas <mssrinivas@users.noreply.github.com>
2021-03-15 22:46:43 -0700

    Update circularqueue.go

--- QueueService/service/circularqueue.go
+++ QueueService/service/circularqueue.go
@@ -1,34 +1,42 @@
-import(
-  v1 "QueueService/contracts"
+package service
+
+import (
+	v1 "QueueService/contracts"
 )
 
 type TaskQueue struct {
-   FrontIndex int  
-   RearIndex  int
-   TaskList   []v1.Task
-   MutexMap   map[string]&sync.Mutex{}
+	FrontIndex int
+	RearIndex  int
+	TaskList   []v1.Task
+	MutexMap   map[string]sync.Mutex
 }
 
-func NewTaskQueue(maxBuffer int) TaskQueue{
-  queue := TaskQueue{
-    FrontIndex: 0,
-    RearIndex: 0,
-    TaskList: make([]v1.Task, maxBuffer),
-    MutexMap:  make([string]&sync.Mutex{}, maxBuffer),
-  }
-  
-  return queue
+func NewTaskQueue(maxBuffer int) TaskQueue {
+	queue := TaskQueue{
+		FrontIndex: 0,
+		RearIndex:  0,
+		TaskList:   make([]v1.Task, maxBuffer),
+		MutexMap:   make([string]sync.Mutex, maxBuffer),
+	}
+	return queue
 }
- 
-// Method to add a task into the queue 
+
+// Method to add a task into the queue
 func (queue TaskQueue) Enqueue(task v1.Task) error {
+	// Add code
+	// Perform the lock on the particular task
+	MutexMap[task.TaskName].Lock()
+	return
 }
- 
+
 // Method to check for the next element to be processed in the queue
-func (queue TaskQueue) PeekQueue() task, error {
+func (queue TaskQueue) PeekQueue() (task, error) {
+	// Add code)
+	return
 }
 
 // Method to remove a processed task from the queue
 func (queue TaskQueue) DeQueue() v1.Task {
-
+	// Add code
+	return
 }
