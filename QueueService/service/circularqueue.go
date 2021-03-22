ab74253057e6cb441de31eda21d1baa09446664f
M.S.Srinivas <mssrinivas@users.noreply.github.com>
2021-03-21 22:54:08 -0700

    Update circularqueue.go

--- QueueService/service/circularqueue.go
+++ QueueService/service/circularqueue.go
@@ -2,42 +2,58 @@ package service
 
 import (
 	v1 "QueueService/contracts"
+	"errors"
+	"sync"
 )
 
+var Mutex *sync.Mutex
 type TaskQueue struct {
-	FrontIndex int
-	RearIndex  int
-	TaskList   []v1.Task
-	MutexMap   map[string]sync.Mutex
-	IndexMap.  map[string]int
+	FrontIndex     int
+	RearIndex      int
+	TaskList       []*v1.Task
+	IndexMap       map[string]int
 }
 
 func NewTaskQueue(maxBuffer int) TaskQueue {
+	Mutex  = &sync.Mutex{}
 	queue := TaskQueue{
-		FrontIndex: 0,
-		RearIndex:  0,
-		TaskList:   make([]v1.Task, maxBuffer),
-		MutexMap:   make([string]sync.Mutex, maxBuffer),
+		FrontIndex:     0,
+		RearIndex:      0,
+		TaskList:       make([]*v1.Task, maxBuffer),
+		IndexMap:		make(map[string]int, maxBuffer),
 	}
 	return queue
 }
 
 // Method to add a task into the queue
-func (queue TaskQueue) Enqueue(task v1.Task) error {
+func (q *TaskQueue) Enqueue(task *v1.Task) (bool, error) {
 	// Add code
 	// Perform the lock on the particular task
-	MutexMap[task.TaskName].Lock()
-	return
+	if q.FrontIndex >= len(q.TaskList) {
+		err := errors.New("queue buffer full")
+		return false, err
+	}
+
+
+	Mutex.Lock()
+	q.TaskList[q.FrontIndex] = task
+	q.IndexMap[task.TaskName] = q.FrontIndex
+	q.FrontIndex++
+	Mutex.Unlock()
+	return true, nil
 }
 
 // Method to check for the next element to be processed in the queue
-func (queue TaskQueue) PeekQueue() (task, error) {
-	// Add code)
-	return
+func (q TaskQueue) PeekQueue() (*v1.Task, error) {
+	if q.TaskList[q.FrontIndex] != nil {
+		return q.TaskList[q.FrontIndex], nil
+	}
+	return nil, nil
 }
 
 // Method to remove a processed task from the queue
-func (queue TaskQueue) DeQueue() v1.Task {
-	// Add code
-	return
+func (q TaskQueue) DeQueue(taskName string) error {
+	index := q.IndexMap[taskName]
+	q.TaskList[index] = nil
+	return nil
 }
